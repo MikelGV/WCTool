04b1f088514f5b7622d3832fa47e4e8096913eda
MikelGv <mikelgaldosv@gmail.com>
2024-08-13 19:04:01 +0200

    filehandler added needs testing

---
+++ fileHandler/fileHandler.go
@@ -0,0 +1,56 @@
+package filehandler
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+type FileHandler struct {
+    // In here i have to add linesCount and others
+    FileName string
+    Count int
+}
+
+func (fh FileHandler) OpenFile() (*os.File, error) {
+    file, err := os.Open(fh.FileName)
+
+    if err != nil {
+        return nil, err
+    }
+
+    return file, nil
+}
+
+func (fh FileHandler) ByteCount() (int, error) {
+    file, err := fh.OpenFile() 
+
+   if  err != nil {
+       log.Fatal(err)
+    }
+
+    defer file.Close()
+
+
+    scanner := bufio.NewScanner(file)
+    scanner.Split(bufio.ScanBytes)
+
+    if err := fh.scan(scanner); err != nil {
+        return 0, err
+    }
+
+    return fh.Count, nil
+}
+
+func (fh *FileHandler) scan(scanner *bufio.Scanner) error {
+    if scanner.Scan() {
+        fh.Count++
+    }
+
+    if err := scanner.Err(); err != nil {
+        return err
+    }
+
+    return nil
+}
+
